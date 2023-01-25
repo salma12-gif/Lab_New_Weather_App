04c0abbe2e8eaa8a29f1429bfb868cc3737760f4
salma12-gif <salekh.pathan@gmail.com>
2023-01-25 23:30:59 +0530

    submitted

--- Weather-Application-main (1)/Weather-Application-main/main.js
+++ Weather-Application-main (1)/Weather-Application-main/main.js
@@ -1,7 +1,8 @@
 const api = {
   key: "7e3f21edee540e6110af347b55eb1ab2",
-  base: "https://api.openweathermap.org/data/2.5/weather"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
+
 //Add an event listener to search-box
 //keypress... call a method
 
